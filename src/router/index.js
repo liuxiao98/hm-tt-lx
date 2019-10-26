6e2afa8052cc9d377c69644196f6e7606aff8ee0
liuxiao98 <you@example.com>
2019-10-26 20:20:42 +0800

    完成登录校检

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,14 @@ import VueRouter from 'vue-router'
 import Vue from 'vue'
 
 import Login from '@/views/login'
+import Home from '@/views/home'
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
   routes: [
-    { path: '/', component: Login }
+    { path: '/login', component: Login },
+    { path: '/', component: Home }
   ]
 })
 
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,19 +1,23 @@
 <template>
-  <div class="l-content">
+  <div class="container">
     <el-card>
-      <img src="../../assets/logo_index.png" alt />
-      <el-form ref="form" :model="form">
-            <el-form-item>
-                <el-input v-model="form.name" placeholder="请输入手机号"></el-input>
-            </el-form-item>
-            <el-form-item>
-                <el-input v-model="form.code" style="width:73%;margin-right:5px" placeholder="请输入验证码"></el-input>
-                <el-button style="width:25%;padding-left:8px" >发送验证码</el-button>
-            </el-form-item>
-            <el-checkbox v-model="checked" style="margin-bottom:20px">我已阅读并同意用户协议和隐私条款</el-checkbox>
-            <el-form-item>
-                <el-button type="primary" style="width:100%">立即登录</el-button>
-            </el-form-item>
+      <!-- logo -->
+      <img src="../../assets/logo_index.png" width="200px" alt />
+      <!-- 表单 -->
+      <el-form ref="loginForm" status-icon :model="loginForm" :rules="loginRules">
+        <el-form-item prop="mobile">
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item prop="code">
+          <el-input v-model="loginForm.code" style="width:235px;margin-right:10px" placeholder="请输入验证码"></el-input>
+          <el-button>发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox :value="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button @click="login" type="primary" style="width:100%">立即登录</el-button>
+        </el-form-item>
       </el-form>
     </el-card>
   </div>
@@ -22,35 +26,64 @@
 <script>
 export default {
   data () {
-    return {
-      checked: true,
-      form: {
-        name: '',
-        code: ''
+    const checkMobile = (rule, value, callback) => {
+      if (/^1[3-9]\d{9}$/.test(value)) {
+        callback()
+      } else {
+        callback(new Error('手机号格式不对'))
       }
+    }
+    return {
+      loginForm: {
+        mobile: '',
+        code: ''
+      },
+      loginRules: {
+        mobile: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          { validator: checkMobile, trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '请输入验证码', trigger: 'blur' },
+          { len: 6, message: '请输入6个字符以上验证码', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    login () {
+      this.$refs['loginForm'].validate((valid) => {
+        if (valid) {
+          this.$http.post('authorizations', this.loginForm).then(res => {
+            this.$router.push('/')
+          }).catch(() => {
+            this.$message.error('手机号或验证码错误')
+          })
+        }
+      })
     }
   }
 }
+
 </script>
 
 <style scoped lang='less'>
-.l-content {
+.container {
   width: 100%;
   height: 100%;
-  background: url("../../assets/timg0.jpg") no-repeat center/ cover;
+  background: url("../../assets/timg0.jpg") no-repeat center / cover;
   position: absolute;
   left: 0;
   top: 0;
 }
-
 .el-card {
-  width: 420px;
-  height: 350px;
+  width: 400px;
+  height: 340px;
   position: absolute;
   left: 50%;
   top: 50%;
   transform: translate(-50%, -50%);
-  img {
+  img{
     display: block;
     margin: 0 auto 30px;
   }
